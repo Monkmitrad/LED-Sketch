53a5db448c1c50191b8c836d7d053e0587deca35
monkmitrad <monkmitrad@gmail.com>
2020-10-07 15:49:17 +0200

    refactored code, input now with variable parameters, added strobe effect

--- LED_Sketch.ino
+++ LED_Sketch.ino
@@ -24,9 +24,8 @@ char tempChars[numChars];        // temporary array for use when parsing
 
 // variables to hold the parsed data
 char messageFromPC[numChars] = {0};
-int integerFromPC1 = 0;
-int integerFromPC2 = 0;
-int integerFromPC3 = 0;
+int integerFromPC[numChars];
+byte integerIndex = 0;
 
 uint32_t color = WHITE;
 
@@ -56,6 +55,7 @@ void setup() {
   flushBuffer();
 
   strip.begin();
+  setAll(1, 1, 1);
   strip.show(); // Initialize the strip to 'off'
   Serial.begin(9600); // opens serial port, sets baud rate to 9600
 }
@@ -65,17 +65,27 @@ void loop() {
   if (newData == true) {
     strcpy(tempChars, receivedChars);
     // this temporary copy is necessary to protect the original data
-    //   because strtok() used in parseData() replaces the commas with \0
+    //  because strtok() used in parseData() replaces the commas with \0
     parseData();
+    newData = false;
     // showParsedData();
     useData();
-    newData = false;
   } else {
-    if (mode == 'R' || mode == 'L') {
-      runningLight();
-    }
-    if (mode == 'F') {
-      colorFade();
+    switch (mode) {
+      case 'R':
+        runningLight();
+        break;
+      case 'L':
+        runningLight();
+        break;
+      case 'F':
+        colorFade();
+        break;
+      case 'S':
+        strobe();
+        break;
+      default:
+        break;
     }
   }
 }
@@ -83,7 +93,7 @@ void loop() {
 void recvWithStartEndMarkers() {
 
   static boolean recvInProgress = false;
-  static byte ndx = 0;
+  static byte index = 0;
   char startMarker = '<';
   char endMarker = '>';
   char rc;
@@ -93,16 +103,16 @@ void recvWithStartEndMarkers() {
 
     if (recvInProgress == true) {
       if (rc != endMarker) {
-        receivedChars[ndx] = rc;
-        ndx++;
-        if (ndx >= numChars) {
-          ndx = numChars - 1;
+        receivedChars[index] = rc;
+        index++;
+        if (index >= numChars) {
+          index = numChars - 1;
         }
       }
       else {
-        receivedChars[ndx] = '\0'; // terminate the string
+        receivedChars[index] = '\0'; // terminate the string
         recvInProgress = false;
-        ndx = 0;
+        index = 0;
         newData = true;
       }
     }
@@ -117,44 +127,50 @@ void parseData() {
   if (newData == true) {
     Serial.print("This just in ... ");
     Serial.println(receivedChars);
-    //newData = false;
 
     char * strtokIndx; // this is used by strtok() as an index
 
     strtokIndx = strtok(tempChars, ",");     // get the first part - the string
     strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+    while (true) {
+      //find the next ','
+      strtokIndx = strtok(NULL, ",");
 
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    integerFromPC1 = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    integerFromPC2 = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    integerFromPC3 = atoi(strtokIndx);     // convert this part to an integer
+      //if not found, we're done
+      if ( strtokIndx == NULL ) {
+        break;
+      }
+      else {
+        //parseValue
+        integerFromPC[integerIndex] = atoi( strtokIndx );
+        if (integerIndex < numChars) {
+          integerIndex++;
+        } else {
+          break;
+        }
+      }
+    }
   }
 }
 
 void showParsedData() {
   Serial.print("Message: ");
   Serial.println(messageFromPC);
-  Serial.print("Integer 1: ");
-  Serial.println(integerFromPC1);
-  Serial.print("Integer 2: ");
-  Serial.println(integerFromPC2);
-  Serial.print("Integer 3: ");
-  Serial.println(integerFromPC3);
+  Serial.println("Integer array");
+  for (int i = 0; i < numChars; i++) {
+    Serial.println(integerFromPC[i]);
+  }
 }
 
 void useData() {
-  if (integerFromPC1 < 0) {
-    integerFromPC1 = 0;
+  if (integerFromPC[0] < 0) {
+    integerFromPC[0] = 0;
   }
-  if (integerFromPC2 < 0) {
-    integerFromPC2 = 0;
+  if (integerFromPC[1] < 0) {
+    integerFromPC[1] = 0;
   }
-  if (integerFromPC3 < 0) {
-    integerFromPC3 = 0;
+  if (integerFromPC[2] < 0) {
+    integerFromPC[2] = 0;
   }
   switch (messageFromPC[0]) {
     case 'C':
@@ -163,48 +179,43 @@ void useData() {
       break;
     case 'V':
       if (mode != 'V') {
-        for (uint16_t i = 0; i < strip.numPixels(); i++) {
-          strip.setPixelColor(i, 0);
-        }
-        strip.show();
+        setAll(0, 0, 0);
         mode = 'V';
       }
       vuMeter();
       break;
     case 'R':
       mode = 'R';
-      integerFromPC1 %= 256;
-      if (integerFromPC1 <= 1) {
-        integerFromPC1 = 2;
+      integerFromPC[0] %= 256;
+      if (integerFromPC[0] <= 1) {
+        integerFromPC[0] = 2;
       }
-      stepTime = integerFromPC1;
-      runningCounter = 0;
+      initializeCounter();
       runningCounter2 = 0;
-      flushBuffer();
       runningLight();
       break;
     case 'L':
       mode = 'L';
-      integerFromPC1 %= 256;
-      if (integerFromPC1 <= 1) {
-        integerFromPC1 = 2;
+      integerFromPC[0] %= 256;
+      if (integerFromPC[0] <= 1) {
+        integerFromPC[0] = 2;
       }
-      stepTime = integerFromPC1;
-      runningCounter = 0;
+      initializeCounter();
       runningCounter2 = 0;
-      flushBuffer();
       runningLight();
       break;
     case 'F':
       mode = 'F';
-      integerFromPC1 %= 256;
-      if (integerFromPC1 <= 1) {
-        integerFromPC1 = 2;
+      integerFromPC[0] %= 256;
+      if (integerFromPC[0] <= 1) {
+        integerFromPC[0] = 2;
       }
-      stepTime = integerFromPC1;
-      runningCounter = 0;
-      flushBuffer();
+      initializeCounter();
       colorFade();
+      break;
+    case 'S':
+      mode = 'S';
+      strobe();
       break;
     default:
       mode = 'C';
@@ -213,56 +224,50 @@ void useData() {
 }
 
 void colorWipe() {
-  integerFromPC1 %= 256;
-  integerFromPC2 %= 256;
-  integerFromPC3 %= 256;
+  integerFromPC[0] %= 256;
+  integerFromPC[1] %= 256;
+  integerFromPC[2] %= 256;
 
-  color = strip.Color(integerFromPC1, integerFromPC2, integerFromPC3);
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, color);
-    strip.show();
-    delay(1);
-  }
+  setAll(integerFromPC[0], integerFromPC[1], integerFromPC[2], 1);
 }
 
 void vuMeter() {
-  if (integerFromPC1 > LEDS / 2) {
-    integerFromPC1 = LEDS / 2;
+  if (integerFromPC[0] > LEDS / 2) {
+    integerFromPC[0] = LEDS / 2;
   }
-  if (integerFromPC2 > LEDS / 2) {
-    integerFromPC2 = LEDS / 2;
+  if (integerFromPC[1] > LEDS / 2) {
+    integerFromPC[1] = LEDS / 2;
   }
-  if (integerFromPC3 > 255 || integerFromPC3 == 0) {
-    integerFromPC3 = 255;
+  if (integerFromPC[2] > 255 || integerFromPC[2] == 0) {
+    integerFromPC[2] = 255;
   }
 
-  uint32_t red = strip.Color(1 * integerFromPC3, 0, 0);
-  uint32_t green = strip.Color(0, 1 * integerFromPC3, 0);
-  uint32_t white = strip.Color(1 * integerFromPC3, 1 * integerFromPC3, 1 * integerFromPC3);
+  uint32_t red = strip.Color(1 * integerFromPC[2], 0, 0);
+  uint32_t green = strip.Color(0, 1 * integerFromPC[2], 0);
+  uint32_t white = strip.Color(1 * integerFromPC[2], 1 * integerFromPC[2], 1 * integerFromPC[2]);
 
   //left channel
 
   //last peak
-  if (integerFromPC1 > lastPeakL) {
+  if (integerFromPC[0] > lastPeakL) {
     //louder
-    for (uint16_t i = lastPeakL; i < integerFromPC1; i++) {
+    for (uint16_t i = lastPeakL; i < integerFromPC[0]; i++) {
       strip.setPixelColor(i, red);
     }
     //strip.show();
   } else {
-    if (integerFromPC1 < lastPeakL) {
+    if (integerFromPC[0] < lastPeakL) {
       //quieter
-      for (uint16_t i = integerFromPC1; i < lastPeakL; i++) {
+      for (uint16_t i = integerFromPC[0]; i < lastPeakL; i++) {
         strip.setPixelColor(i, 0);
       }
-      //strip.show();
     }
   }
-  lastPeakL = integerFromPC1;
+  lastPeakL = integerFromPC[0];
 
   //staying peak
-  if (integerFromPC1 >= maxPeakL) {
-    maxPeakL = integerFromPC1;
+  if (integerFromPC[0] >= maxPeakL) {
+    maxPeakL = integerFromPC[0];
     peakCounterL = 0;
   } else {
     if (peakCounterL > 4) {
@@ -273,68 +278,59 @@ void vuMeter() {
     }
   }
   strip.setPixelColor(maxPeakL, WHITE);
-  //strip.show();
-
   //right channel
 
   //last peak
-  if (integerFromPC2 > lastPeakR) {
+  if (integerFromPC[1] > lastPeakR) {
     //louder
-    for (uint16_t i = strip.numPixels() - lastPeakR; i > strip.numPixels() - integerFromPC2; i--) {
+    for (uint16_t i = LEDS - lastPeakR; i > LEDS - integerFromPC[1]; i--) {
       strip.setPixelColor(i, green);
     }
     //strip.show();
   } else {
-    if (integerFromPC2 < lastPeakR) {
+    if (integerFromPC[1] < lastPeakR) {
       //quieter
-      for (uint16_t i = strip.numPixels() - integerFromPC2; i > strip.numPixels() - lastPeakR; i--) {
+      for (uint16_t i = LEDS - integerFromPC[1]; i > LEDS - lastPeakR; i--) {
         strip.setPixelColor(i, 0);
       }
-      //strip.show();
     }
   }
-  lastPeakR = integerFromPC2;
+  lastPeakR = integerFromPC[1];
 
   //staying peak
-  if (integerFromPC2 >= maxPeakR) {
-    maxPeakR = integerFromPC2;
+  if (integerFromPC[1] >= maxPeakR) {
+    maxPeakR = integerFromPC[1];
     peakCounterR = 0;
   } else {
     if (peakCounterL > 4) {
-      strip.setPixelColor(strip.numPixels() - maxPeakR, 0);
+      strip.setPixelColor(LEDS - maxPeakR, 0);
       maxPeakR--;
     } else {
       peakCounterR++;
     }
   }
-  strip.setPixelColor(strip.numPixels() - maxPeakR, WHITE);
+  strip.setPixelColor(LEDS - maxPeakR, WHITE);
   strip.show();
-}
-
-void flushBuffer() {
-  for (uint16_t i = 0; i < stripBuffer.capacity; i++) {
-    stripBuffer.push(0);
-  }
 }
 
 void runningLight() {
   if (runningCounter % stepTime == 0) { //don't make a step each loop (speed)
     stripBuffer.pop();
-    stripBuffer.unshift(runningPattern[runningCounter2 % (sizeof(runningPattern) / sizeof(uint32_t))]);
+    stripBuffer.unshift(runningPattern[runningCounter2 % (sizeof(runningPattern) / sizeof(long))]);
     runningCounter2++;
   }
   runningCounter++;
 
   switch (mode) {
     case 'R':
-      for (uint16_t i = 0; i < strip.numPixels(); i++) {
+      for (byte i = 0; i < LEDS; i++) {
         strip.setPixelColor(i, stripBuffer[i]);
       }
       strip.show();
       delay(10);
       break;
     case 'L':
-      for (uint16_t i = strip.numPixels(); i > 0; i--) {
+      for (byte i = LEDS; i > 0; i--) {
         strip.setPixelColor(i - 1, stripBuffer[strip.numPixels() - 1 - i]);
       }
       strip.show();
@@ -357,11 +353,52 @@ void colorFade() {
       fadeB--;
       fadeR++;
     }
-    uint32_t fadeColor = strip.Color(fadeR, fadeG, fadeB);
-    for (uint16_t i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, fadeColor);
-    }
-    strip.show();
+    setAll(fadeR, fadeG, fadeB);
   }
   runningCounter++;
+}
+
+void strobe() {
+  integerFromPC[0] %= 256;  //flashCount
+  integerFromPC[1] %= 256;  //flashDelay
+  integerFromPC[2] %= 10000;  //flashPause
+
+  for (byte i = 0; i < integerFromPC[0]; i++) {
+    setAll(255, 255, 255);
+    delay(integerFromPC[1]);
+    setAll(0, 0, 0);
+    delay(integerFromPC[1]);
+  }
+  delay(integerFromPC[2]);
+}
+
+//general functions
+
+void setAll(byte colorR, byte colorG, byte colorB) {
+  long color = strip.Color(colorR, colorG, colorB);
+  for (byte i = 0; i < LEDS; i++) {
+    strip.setPixelColor(i, color);
+  }
+  strip.show();
+}
+
+void setAll(byte colorR, byte colorG, byte colorB, byte speed) {
+  long color = strip.Color(colorR, colorG, colorB);
+  for (byte i = 0; i < LEDS; i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(speed);
+  }
+}
+
+void flushBuffer() {
+  for (byte i = 0; i < stripBuffer.capacity; i++) {
+    stripBuffer.push(0);
+  }
+}
+
+void initializeCounter() {
+  stepTime = integerFromPC[0];
+  runningCounter = 0;
+  flushBuffer();
 }
